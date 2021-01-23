bf38b67a1ca9891738e008689a49a0afe15baabf
Samuel Shard <samuel.shard@ontariotechu.net>
2021-01-23 12:24:22 -0500

    Added another sentence to the first print statement using the master branch.

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -2,7 +2,8 @@
 // Samuel Shard - 100745640
 public class lab01 {
 	public static void main(String args[]) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world!  This sentence was added from the master branch.");
 		System.out.println("This second line was added from the cloned repository.");
+
 	}
 }
