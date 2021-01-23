9d91c3b16932c8cb2db2fafe297c8baab0ab6d67
Samuel Shard <samuel.shard@ontariotechu.net>
2021-01-23 12:21:20 -0500

    Added a third print statement from the beta branch.

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -4,5 +4,7 @@ public class lab01 {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
 		System.out.println("This second line was added from the cloned repository.");
+		System.out.println("This third line was added in the beta branch.");
+
 	}
 }
