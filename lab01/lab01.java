6ec46ce01b4131d7293f934ac17c17d816d2994d
Samuel Shard <samuel.shard@ontariotechu.net>
2021-01-23 11:27:26 -0500

    Added a second print statement to the cloned repo.

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -3,5 +3,6 @@
 public class lab01 {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
+		System.out.println("This second line was added from the cloned repository.");
 	}
 }
