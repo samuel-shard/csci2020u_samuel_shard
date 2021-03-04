8ac1efea6b5a1be24231bb1b876c71749a4b8328
Samuel Shard <samuel.shard@ontariotechu.net>
2021-03-03 21:25:41 -0500

    Completed Lab 5 with .gitignore file to ignore binary files.

---
+++ lab06/src/csci2020u/lab06/Main.java
@@ -0,0 +1,41 @@
+package csci2020u.lab06;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import static javafx.scene.text.Font.font;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("lab06.fxml"));
+        primaryStage.setTitle("Lab 06");
+//        Creating myScene with custom layout
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
